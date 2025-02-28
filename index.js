1b0bf75e8ca6be111769cb10bc30a1c29b06045c
Racheal Späni <37956115+xomen85@users.noreply.github.com>
2025-02-28 13:57:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const imageContainerEl = document.querySelector(".image-container")
+
+const btnEl = document.querySelector(".btn")
+
+btnEl.addEventListener("click", ()=>{
+imageNum = 10;
+   addNewImages()
+    });
+
+function addNewImages() {
+    for (let index = 0; index < imageNum; index++) {
+        const newImageEl = document.createElement("img")
+    newImageEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random() *2000)}`;
+    imageContainerEl.appendChild(newImageEl)
+        
+    }
+    
+}
